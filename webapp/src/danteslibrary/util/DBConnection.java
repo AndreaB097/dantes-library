e5033cc8957e5635b62d8a30faa8d02bed55ce54
hidingbit <37928190+hidingbit@users.noreply.github.com>
2019-12-19 16:15:09 +0100

    Primo commit del sito

---
+++ webapp/src/danteslibrary/util/DBConnection.java
@@ -0,0 +1,62 @@
+package danteslibrary.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DBConnection  {
+
+	private static List<Connection> freeDbConnections;
+
+	static {
+		freeDbConnections = new LinkedList<Connection>();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("DB driver not found:"+ e.getMessage());
+		} 
+	}
+	
+	private static synchronized Connection createDBConnection() throws SQLException {
+		Connection newConnection = null;
+
+		String db_name = "danteslibrary";
+		String db_username = "root";
+		String db_password = "LaMiaPassword1!";
+		String url = "jdbc:mysql://localhost:3306/"+ db_name + "?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true"
+				+ "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		
+		newConnection = DriverManager.getConnection(url, db_username, db_password);
+
+		newConnection.setAutoCommit(true);
+		return newConnection;
+	}
+
+
+	public static synchronized Connection getConnection() throws SQLException {
+		Connection connection;
+
+		if (!freeDbConnections.isEmpty()) {
+			connection = (Connection) freeDbConnections.get(0);
+			freeDbConnections.remove(0);
+
+			try {
+				if (connection.isClosed())
+					connection = getConnection();
+			} catch (SQLException e) {
+				connection.close();
+				connection = getConnection();
+			}
+		} else {
+			connection = createDBConnection();		
+		}
+
+		return connection;
+	}
+
+	public static synchronized void releaseConnection(Connection connection) throws SQLException {
+		if(connection != null) freeDbConnections.add(connection);
+	}
+}
